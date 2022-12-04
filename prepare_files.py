b75b648c048edf9c163683f41ee027b99accfa6d
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-12-04 18:46:32 +0100

    update 4/12

--- prepare_files.py
+++ prepare_files.py
@@ -2,8 +2,8 @@ import os
 
 year = 2022
 
-for i in range(1,26):
-    path = os.path.dirname(__file__) + "/{}/Day_".format(year) + str(i)
+for i in range(5,26):
+    path = os.path.dirname(__file__) + "/{}/Day_".format(year) + str(i) if i >= 10 else os.path.dirname(__file__) + "/{}/Day_0".format(year) + str(i)
     if not os.path.exists(path):
         os.makedirs(path)
         file = open(path + "/input.txt", "w")
